2e081911b478e41e6e9f937cea7856d6fa6cc9a6
Adrian <adrianbailador@hotmail.com>
2020-12-13 14:33:55 +0000

    Initial commit

---
+++ Mislugares/src/main/java/android/app/mislugares/Principal.kt
@@ -0,0 +1,22 @@
+package android.app.mislugares
+
+import com.example.mislugares.Lugar
+
+
+internal object Principal {
+    @JvmStatic
+    fun main(main: Array<String>) {
+        val lugar = Lugar(
+            "Escuela Politécnica Superior de Gandía",
+            "C/ Paranimf, 1 46730 Gandia (SPAIN)",
+            GeoPunto(-0.166093, 38.995656),
+            "",
+            962849300,
+            "http://www.epsg.upv.es",
+            "Uno de los mejores lugares para formarse.",
+            System.currentTimeMillis(),
+            3F
+        )
+        println("Lugar $lugar")
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MisLugaresKotlin
+  Application in which we can register the places we have recently visited.
